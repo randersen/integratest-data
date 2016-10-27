f8b9df275db7f491d519f49de62557819e31066d
randersen <ryan.andersen91@gmail.com>
2016-10-26 23:42:17 -0600

    created context provider, tableCreator, and first couple of scritps that create the accounts and testCase tables

---
+++ Integratest.Data/Integratest.Data.Tests/ContextTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Integratest.Data.Providers;
+using Integratest.Data.Tables;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Integratest.Data.Tests
+{
+    [TestClass]
+    public class ContextTests
+    {
+
+        [TestMethod]
+        public void Test()
+        {
+            TestCaseTable.CreateTable();
+            AccountsTable.CreateTable();
+        }
+
+    }
+}
---
+++ Integratest.Data/Integratest.Data/Providers/DynamoDbContextProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using System.Security;
+using Amazon;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.Runtime;
+
+namespace Integratest.Data.Providers
+{
+    public class DynamoDbContextProvider
+    {
+        private static string _awsUsername = Environment.GetEnvironmentVariable("INTEAGRATEST_AWS_USERNAME");
+
+        private static readonly string _awsPassword = Environment.GetEnvironmentVariable("INTEGRATEST_AWS_PASSWORD");
+
+        private static AmazonDynamoDBClient _client
+            => new AmazonDynamoDBClient(new BasicAWSCredentials(_awsUsername, _awsPassword), RegionEndpoint.USWest2);
+
+
+        public static DynamoDBContext CurrentContext => new DynamoDBContext(_client);
+        public static AmazonDynamoDBClient CurrentClient => _client;
+    }
+}
